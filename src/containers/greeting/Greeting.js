d11ff973934e18d56cfd931914e99a97c387ffb7
lucky <lucky.108@ntescapelabs.com>
2024-10-09 18:55:36 +0530

    chng

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,8 +41,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./luckyresume.pdf")}
+                    download="LuckyResume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
